12824e92258a446772c11263e71127cf93f574f4
Abhinav1530 <62808474+Abhinav9453@users.noreply.github.com>
2024-09-11 15:40:46 +0000

    demo changes

---
+++ 01_Basic/string.js
@@ -0,0 +1,7 @@
+const name = "Abhinav";
+const repocount = 15;
+
+//console.log(name+repocount);
+console.log( `My name is ${name} and my repocount is ${repocount}`);
+ 
+const gamename = new String("Abhinavsi");
