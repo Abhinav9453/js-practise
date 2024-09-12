39195e9c3be6adb737c3708da918d009e9884da9
Abhinav1530 <62808474+Abhinav9453@users.noreply.github.com>
2024-09-12 19:09:46 +0000

    Demo changes

--- 01_Basic/numbersandmaths.js
+++ 01_Basic/numbersandmaths.js
@@ -5,3 +5,29 @@ const balance = new Number (100)
 console.log (balance);
 
 console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const newNumber = 123.897;
+console.log(newNumber.toPrecision(4));
+
+const hundread = 10000000
+console.log(hundread.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
